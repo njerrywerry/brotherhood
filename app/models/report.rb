c7359cb575dead4d2b301594cbaa078b67eb354f
Sylvia <sylviakieha@gmail.com>
2015-12-05 22:15:48 +0300

    added validations to report model

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,5 +1,9 @@
 class Report < ActiveRecord::Base
   belongs_to :category
+  validates :title, presence: true
+  validates :details, presence: true
+  validates :phone_no, presence: true, numericality: true, length: { is: 10, message: "is not valid" } 
+  validates :image, presence: true
 
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
